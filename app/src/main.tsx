e9e091c08a23ff9197bc16e117d37726bb09849c
terumitt <f4k0m6r0@icloud.com>
2025-06-15 16:11:29 +0900

    main.tsxの記述を変更

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+// src/main.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import Home from './pages/Home.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>トップページ</div>,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+  },
+])
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
