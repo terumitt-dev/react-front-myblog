02a1cd201b00eeaa4ff43f058ee29adcc5fe12f6
terumitt <f4k0m6r0@icloud.com>
2025-06-15 22:43:18 +0900

    main.tsxとapp.tsxの記述変更

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,29 +1,11 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
+// app/src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Top from './pages/Top'
-import Hobby from './pages/category/Hobby'
-import Tech from './pages/category/Tech'
-import Other from './pages/category/Other'
-
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Top />} />
-      <Route path="/category/hobby" element={<Hobby />} />
-      <Route path="/category/tech" element={<Tech />} />
-      <Route path="/category/other" element={<Other />} />
-    </Routes>
-  </BrowserRouter>
-)
-
-export default App
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
