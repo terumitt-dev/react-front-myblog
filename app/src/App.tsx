5465a085440b00d35c336848cdb2217c5b5741b6
terumitt <f4k0m6r0@icloud.com>
2025-06-22 01:11:24 +0900

    Homeのページを削除

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Top from '@/pages/Top'
-import Home from '@/pages/Home'
 import Category from '@/pages/Category'
 import PostDetail from '@/pages/PostDetail'
-import Admin from '@/pages/Admin' // ← 追加！
+import Admin from '@/pages/Admin'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Top />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/category/:category" element={<Category />} />
         <Route path="/posts/:id" element={<PostDetail />} />
         <Route path="/admin" element={<Admin />} /> {/* ← 追加！ */}
