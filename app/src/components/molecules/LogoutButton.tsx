f1b593791f8d104af0692a0590ee577b084f4d30
terumitt <f4k0m6r0@icloud.com>
2025-07-09 19:23:22 +0900

    ログアウトのボタンタイプの記述を追加

--- app/src/components/molecules/LogoutButton.tsx
+++ app/src/components/molecules/LogoutButton.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 const LogoutButton = ({ onClick }: Props) => {
   return (
-    // biome-ignore lint/a11y/useButtonType: <explanation>
     <button
+      type="button"
       onClick={onClick}
       className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition text-sm"
     >
