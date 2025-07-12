5a89b845328fc3ec7581ed1b3664807c5f54cbde
terumitt <f4k0m6r0@icloud.com>
2025-07-12 14:11:05 +0900

    不要になったSubmitCommentButton.tsxを削除

--- app/src/components/molecules/SubmitCommentButton.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/molecules/SubmitCommentButton.tsx
-import Button from '@/components/atoms/Button';
-
-const SubmitCommentButton = ({ onSubmit }: { onSubmit: () => void }) => (
-  <Button label="コメントを確定する" onClick={onSubmit} variant="primary" />
-);
-
-export default SubmitCommentButton;
