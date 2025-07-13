601bcc362cf06108a8fbda976a759822c61624cb
terumitt <f4k0m6r0@icloud.com>
2025-07-14 00:12:38 +0900

    CommentButtonの記述変更

--- app/src/components/molecules/CommentButtons.tsx
+++ app/src/components/molecules/CommentButtons.tsx
@@ -4,14 +4,23 @@ import Button from '@/components/atoms/Button'
 type Props = {
   onSubmit: () => void
   onCancel: () => void
-  className?: string
 }
 
 const CommentButtons = ({ onSubmit, onCancel }: Props) => {
   return (
-    <div className="flex gap-2 justify-end">
-      <Button label="コメントを確定する" onClick={onSubmit} variant="primary" className="flex-1"/>
-      <Button label="コメントしない" onClick={onCancel} variant="secondary"/>
+    <div className="flex flex-col sm:flex-row gap-2 justify-end">
+      <Button
+        label="コメントを確定する"
+        onClick={onSubmit}
+        variant="primary"
+        className="w-full sm:basis-[60%]"
+      />
+      <Button
+        label="コメントしない"
+        onClick={onCancel}
+        variant="secondary"
+        className="w-full sm:basis-[40%]"
+      />
     </div>
   )
 }
