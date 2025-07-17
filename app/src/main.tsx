5505663bfb91ade8365217e9c9f5f0cc89b1614c
terumitt <f4k0m6r0@icloud.com>
2025-07-17 18:52:37 +0900

    mswを使えるように調整

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,27 +1,4 @@
 // app/src/main.tsx
-/** biome-ignore-all lint/style/noNonNullAssertion: <explanation> */
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import './index.css'
-
-// // MSWのセットアップを含む関数を定義
-// const prepare = async () => {
-//   if (import.meta.env.DEV) {
-//     const { worker } = await import('./mocks/browser')
-//     await worker.start()
-//   }
-
-//   ReactDOM.createRoot(document.getElementById('root')!).render(
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>
-//   )
-// }
-
-// prepare()
-
-// app/src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -33,6 +10,7 @@ const prepare = async () => {
     await worker.start()
   }
 
+  // biome-ignore lint/style/noNonNullAssertion: <explanation>
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <App />
@@ -41,3 +19,16 @@ const prepare = async () => {
 }
 
 prepare()
+
+// // app/src/main.tsx
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
+
+// // biome-ignore lint/style/noNonNullAssertion: <explanation>
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,19 +1,6 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// // biome-ignore lint/style/useNodejsImportProtocol: <explanation>
-// import path from 'path'; // ← 追加！
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//     },
-//   },
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
 import path from 'path';
 
 export default defineConfig({
