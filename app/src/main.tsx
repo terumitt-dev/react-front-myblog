cb4295c47ca8ffc54160a7bd4ff5c801b1f159aa
terumitt <f4k0m6r0@icloud.com>
2025-07-17 17:46:35 +0900

    白くサイトが表示されるので調整

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,11 +1,32 @@
 // app/src/main.tsx
 /** biome-ignore-all lint/style/noNonNullAssertion: <explanation> */
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
+
+// // MSWのセットアップを含む関数を定義
+// const prepare = async () => {
+//   if (import.meta.env.DEV) {
+//     const { worker } = await import('./mocks/browser')
+//     await worker.start()
+//   }
+
+//   ReactDOM.createRoot(document.getElementById('root')!).render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   )
+// }
+
+// prepare()
+
+// app/src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// MSWのセットアップを含む関数を定義
 const prepare = async () => {
   if (import.meta.env.DEV) {
     const { worker } = await import('./mocks/browser')
