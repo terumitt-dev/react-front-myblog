e014354a799309268c5c4db9c1c177fb336ae828
terumitt <f4k0m6r0@icloud.com>
2025-08-09 17:06:27 +0900

    tailwind.config.jsの不要な記述削除

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,11 +1,7 @@
 // tailwind.config.js
 module.exports = {
   darkMode: "class",
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./app/src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
