cc0ec1217c8c8e4f5ac9e79aca98d2200fb09b86
terumitt <f4k0m6r0@icloud.com>
2025-09-13 23:07:02 +0900

    アニメ層の重なりを解消できるように調整

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -4,6 +4,16 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      zIndex: {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        "-1": "-1",
+        "-2": "-2",
+        "-3": "-3",
+      },
       colors: {
         category: {
           hobby: {
